c39997762e496f04783ce6a53330c741e29faf83
Karlos97chss <sanchescarlosh@gmail.com>
2024-10-17 17:10:40 -0300

    Esqueleto da Calculadora

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-green-600 text-white p-4 text-center">
+      <p>&copy; 2024 GreenGas. Todos os direitos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,35 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // Adicione novas cores ou sobrescreva as existentes
+        green: {
+          light: '#6ee7b7',
+          DEFAULT: '#10b981',
+          dark: '#047857',
+        },
+        gray: {
+          light: '#f3f4f6',
+          DEFAULT: '#9ca3af',
+          dark: '#374151',
+        },
+      },
+      fontFamily: {
+        // Adicionar fontes personalizadas
+        sans: ['Inter', 'sans-serif'],
+      },
+      spacing: {
+        // Adicionar espaçamentos personalizados
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
       },
     },
   },
   plugins: [],
 };
+
