6d9286f51091da6b87adaf84b7326845ca77cb5f
unknown <robsontec.viana79@gmail.com>
2024-11-20 18:22:16 -0300

    projeto_estruturado

---
+++ Historico_Fazendas/front/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+    return (
+      <footer>
+        <p>&copy; 2024 Biodigestor Inc. Todos os direitos reservados.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
