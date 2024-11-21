4d174dbd4caad6a4467558407b43c8ddc187fab2
Karlos97chss <sanchescarlosh@gmail.com>
2024-11-21 20:43:52 -0300

    Atualização para React

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-green-600 text-white p-4 text-center">
-      <p>&copy; 2024 GreenGas. Todos os direitos reservados.</p>
-    </footer>
-  );
-};
-
-export default Footer;
