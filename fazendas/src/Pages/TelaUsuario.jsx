6ec93f25aff00d20e06abffafc77b8acf66ad7cb
Isabela Oliveira <costbela02@gmail.com>
2024-12-04 19:57:41 -0300

    CSS da Tela do Usuario

--- fazendas/src/Pages/TelaUsuario.jsx
+++ fazendas/src/Pages/TelaUsuario.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import Menu from "../components/Menu";
 import Footer from "../components/Footer";
+import "./TelaUsuario.css"
 
 function TelaUsuario() {
   return (
