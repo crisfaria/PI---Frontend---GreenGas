9203df5bf12d1364f8f7c1639048bc41ba3b93ba
Karlos97chss <sanchescarlosh@gmail.com>
2024-12-04 20:52:22 -0300

    Atualização 1.0.4

--- Calculadora/src/components/Header.jsx
+++ Calculadora/src/components/Header.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import TelaUsuario from "../Pages/TelaUsuario";
 import { NavLink } from "react-router-dom";
-import logo from "../image/logo.png"
+import logo from "../image/logo.png";  // Imagem da logo
 import "./Header.css"; // Importando o CSS para o header
 
 function Header() {
   return (
     <header className="header">
       <div className="header-logo">
-        <img src="../src/image/logo.png" alt="Logo" /> {/* Logo da empresa ou sistema */}
+        <img src={logo} alt="Logo" /> {/* Logo da empresa ou sistema */}
       </div>
       <nav className="header-nav">
         <NavLink to="/Perfil" className="nav-link">Perfil</NavLink>
         <NavLink to="/fazendas" className="nav-link">Fazendas</NavLink>
-        <NavLink to="/" className="nav-link">Calculadora</NavLink>
+        <NavLink to="" className="nav-link">Calculadora</NavLink>
         <NavLink to="/relatorio" className="nav-link">Histórico</NavLink>
-        <NavLink to="/sair" className="logout">Logout</NavLink>
       </nav>
     </header>
   );
